0ca722fe6d4d26dfa910452171cc659b0891412e
ferreis <rafa_ferreis@hotmail.com>
2024-01-22 19:53:35 -0300

    teste de commit git kraken

---
+++ begin.php
@@ -0,0 +1,13 @@
+<!-- Começo das aulas de PHP pela alura -->
+
+<!-- comentario em HTML -->
+<?php
+/*
+Assim posso comentar em multplas linhas em PHP
+Assim posso comentar em multplas linhas em PHP
+*/
+
+// comentario de uma linha em PHP
+# comentario de uma linha PHP
+    echo 'Boas-vindas ao universo do PHP!' . PHP_EOL;
+    echo 'Olá mundp!'. PHP_EOL;
